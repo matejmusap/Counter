f5b005c2ba3abd36ab5b1278667e07ced030ef1e
matejmusap <matej.musap@gmail.com>
2018-10-17 12:20:29 +0200

    define function

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 const initialState = {
-    count: 0
+  count: 0
 };
 
 function rootReducer(state = initialState, action) {
-    switch (action.type) {
-      case "INCREMENT":
-        var newState = { ...state };
-        newState.count++;
-        return newState;
-      case "DECREMENT":
-        var newState = { ...state };
-        newState.count--;
-        return newState;
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case "INCREMENT":
+      var newState = { ...state };
+      newState.count++;
+      return newState;
+    case "DECREMENT":
+      var newState = { ...state };
+      newState.count--;
+      return newState;
+    default:
+      return state;
   }
+}
 
 const store = Redux.createStore(rootReducer);
 
-$(document).ready(function() {
-    let currentState = store.getState();
-    $("#counter").text(currentState.count);
-    $("#increment").on("click", function() {
-        store.dispatch({
-            type:"INCREMENT"
-        });
-    
-    let currentState = store.getState();
-    $("#counter").text(currentState.count);
-    });
+function currentState() {
+  let currentState = store.getState();
+  $("#counter").text(currentState.count);
+}
 
-    $("#decrement").on("click", function() {
-        store.dispatch({
-            type:"DECREMENT"
-        });
-    let currentState = store.getState();
-    $("#counter").text(currentState.count);
-    })
-})
+$(document).ready(function() {
+  currentState();
+  $("#increment").on("click", function() {
+    store.dispatch({
+      type: "INCREMENT"
+    });
+    currentState();
+  });
+  $("#decrement").on("click", function() {
+    store.dispatch({
+      type: "DECREMENT"
+    });
+    currentState();
+  });
+});
